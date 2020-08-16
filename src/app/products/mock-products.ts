2ce9424409e540afc53e9f72021ab152f8323687
Sriram <sriram.sridharan@klm.com>
2020-08-16 07:41:28 +0200

    Feat: Added some mock products

--- src/app/products/mock-products.ts
+++ src/app/products/mock-products.ts
@@ -7,6 +7,26 @@ export const MOCK_PRODUCTS: Products[] = [
     imagePath: '/../../assets/Mango.jpg',
     description: 'Sweet Mangoes from Coutrallam',
     price: 300,
-
+  },
+  {
+    id: 2,
+    name: 'Apple',
+    imagePath: '/../../assets/Apple.jpg',
+    description: 'Apples from Indian Himalayan',
+    price: 400,
+  },
+  {
+    id: 3,
+    name: 'Grapes',
+    imagePath: '/../../assets/Grapes.jpg',
+    description: 'Grapes from Ooty',
+    price: 150,
+  },
+  {
+    id: 4,
+    name: 'Banana',
+    imagePath: '/../../assets/Banana.jpg',
+    description: 'Naturally Riped Banana from Kerala',
+    price: 150,
   }
 ];
